44c1a3dda578640b731ebcec2369eb052f16ac7b
grahamliphts_aorus <pornin@hotmail.fr>
2017-12-04 19:58:52 +0100

    update object rotation

--- Jean michel Melun/Assets/Script/InteractibleObject.cs
+++ Jean michel Melun/Assets/Script/InteractibleObject.cs
@@ -6,9 +6,19 @@ public class InteractibleObject : MonoBehaviour {
 
     [SerializeField]
     private int interest;
+
+    public bool animated = true;
+
+    private float maxRotate = 0.30f;
+    private bool left = true;
 	// Use this for initialization
 	void Start () {
-		
+        if (Random.Range(0, 2) == 0)
+            left = false;
+        else
+            left = true;
+        transform.rotation = Quaternion.Euler(0,0,Random.Range(0,30));
+        StartCoroutine(rotate());
 	}
 	
 	// Update is called once per frame
@@ -19,5 +29,21 @@ public class InteractibleObject : MonoBehaviour {
     public int getInterest()
     {
         return interest;
+    }
+
+    IEnumerator rotate()
+    {
+        if (left)
+            transform.Rotate(0, 0, 2);
+        else
+            transform.Rotate(0, 0, -2);
+
+        yield return new WaitForSeconds(0.01f);
+        
+        if(Mathf.Abs(transform.rotation.z) > maxRotate)
+        {
+            left = !left;
+        }
+        StartCoroutine(rotate());
     }
 }
